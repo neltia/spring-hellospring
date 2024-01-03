5ad5fab722908c261a5cea4de5cda23c9f6e9258
neltia <dsz08082@naver.com>
2024-01-03 15:19:33 +0900

    section. 회원 관리 예제 - 자동 의존관계 설정
    MemberController에서 Autowired에 의해 스프링이 DI하는데 memberService이 스프링 빈으로 등록돼 있지 않음 (Controller는 자동 등록)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -12,16 +12,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // MemberService와 MemberRepository가 각 인스턴스를 생성하던 기존 코드 방식에서,
     // 의존주입(DI)을 통해 서비스에서도 같은 MemoryMemberRepository가 사용되도록
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
